b9eeb702a64193142771e3fa8bfd305edc76c61a
melodichand <melodicbusiness@yahoo.com>
2013-10-08 17:07:53 -0400

    Update Gildorym.java

--- src/com/gildorymrp/gildorym/Gildorym.java
+++ src/com/gildorymrp/gildorym/Gildorym.java
@@ -1,16 +1,34 @@
 package com.gildorymrp.gildorym;
 
+import java.util.Random;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Gildorym extends JavaPlugin {
 	
 	public void onEnable() {
 		this.getCommand("newcharacter").setExecutor(new NewCharacterCommand());
-		this.getCommand("surname").setExecutor(new SurnameCommand());
+		this.getCommand("setname").setExecutor(new SetnameCommand());
 		this.getCommand("rollinfo").setExecutor(new RollInfoCommand());
 		this.getCommand("roll").setExecutor(new RollCommand());
-		//this.getServer().getPluginManager().registerEvents(new ClassChangeListener(), this);
-		this.getServer().getPluginManager().registerEvents(new EntityDamageByEntityListener(), this);
+		this.getServer().getPluginManager().registerEvents(new EntityDamageByEntityListener(this), this);
+		this.getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
 	}
+
+	public boolean doDurabilityDamage(int durability, int maxDurability, int damage) {
+		Random randomGenerator = new Random();
+
+		int dieSize = maxDurability - durability;
+		int die = randomGenerator.nextInt(dieSize) + 1;
+		
+		//damaged.sendMessage("dieSize: " + dieSize + " die: " + die);
+			if (die <= damage || die == 1) {
+				return true;
+			} else {
+				return false;
+			}
+	}
+	
+	
 
 }
