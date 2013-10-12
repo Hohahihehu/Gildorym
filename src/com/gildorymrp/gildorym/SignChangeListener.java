52fe42074a26e15d81ecf44447d22416f289bcf9
melodichand <melodicbusiness@yahoo.com>
2013-10-12 08:31:10 -0400

    Implementing boat signs' SignChandListener

---
+++ src/com/gildorymrp/gildorym/SignChangeListener.java
@@ -0,0 +1,22 @@
+package me.lucariatias.plugins.boatsigns;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class SignChangeListener implements Listener {
+
+	@EventHandler
+	public void onSignChange(SignChangeEvent event) {
+		if (event.getLine(0).equalsIgnoreCase("[boat]")) {
+			if (event.getPlayer().hasPermission("boatsigns.create")) {
+				event.setLine(0, ChatColor.BLUE + "[boat]");
+			} else {
+				event.getBlock().breakNaturally();
+				event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to create boat signs!");
+			}
+		}
+	}
+
+}
